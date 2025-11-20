dd781c76db7efacff5c7ac6aa2eaa491cb2e2cce
amphetam1ne <amphetam1ne@proton.me>
2025-11-20 19:29:42 +0200

    add db in config

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class BotSettings(BaseSettings):
     bot_token: str
 
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
